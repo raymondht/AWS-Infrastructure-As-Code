c5944b2af10fab75187f70e0b8f57699502583b5
Erayus <raymondhieutran@gmail.com>
2020-05-14 11:19:45 +1000

    Removed practice stacks

--- hit-me-app/bin/cdk-practice.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from '@aws-cdk/core';
-import { createCdkPracticeStack } from '../lib/cdk-practice-stack';
-
-const app = new cdk.App();
-createCdkPracticeStack(app, 'CdkPracticeStack');
