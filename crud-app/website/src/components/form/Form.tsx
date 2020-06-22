7166c39eab3f3967e051068e503bc13e8e3a0fae
Erayus <raymondhieutran@gmail.com>
2020-06-23 00:25:55 +1000

    Added form component

--- crud-app/website/src/components/form/Form.tsx
+++ crud-app/website/src/components/form/Form.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react'
+import React, { useState, FormEvent } from 'react'
 import { MDBContainer, MDBRow, MDBCol, MDBBtn } from 'mdbreact';
-
+import axios from 'axios';
 
 
 interface IProps {
@@ -11,26 +11,41 @@ interface ICar {
     model: string
 }
 const Form: React.FC<IProps> = (props) => {
-    const [car, setCar] = useState<ICar>();
+    const [car, setCar] = useState<ICar>({
+        brand: '',
+        model: ''
+    });
     
-    const submitHandler = () => {
-        
+    const submitHandler = (e: any) => {
+        e.preventDefault();
+        axios.post('https://kewaszuahd.execute-api.ap-southeast-2.amazonaws.com/prod/cars', {
+            ...car
+        }).then(
+            (response) => console.log(response))
+          .catch(
+              (err) => console.log(err)
+        );
+    }
+
+    const inputHandler = (e: FormEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
+        const {name, value} = e.currentTarget;
+        setCar({...car, [name]: value})
     }
 
     return (
         <MDBContainer style={{border: '1px solid', padding: '15px'}}>
             <MDBRow>
                 <MDBCol md="12">
-                <form>
+                <form onSubmit={submitHandler}>
                     <label htmlFor="defaultFormRegisterEmailEx" className="grey-text">
                         Brand
                     </label>
-                    <input type="text" id="defaultFormRegisterNameEx" className="form-control" />
+                    <input type="text" id="defaultFormRegisterNameEx" className="form-control" name="brand" onChange={inputHandler} />
                     <br />
                     <label htmlFor="defaultFormRegisterEmailEx" className="grey-text">
                         Model
                     </label>
-                    <input type="email" id="defaultFormRegisterEmailEx" className="form-control" />
+                    <input type="text" id="defaultFormRegisterEmailEx" className="form-control" name="model" onChange={inputHandler} />
                     
                     <div className="text-center mt-4">
                     <MDBBtn color="unique" type="submit">
